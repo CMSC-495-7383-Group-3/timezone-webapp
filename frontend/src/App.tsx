8c52f488ba4154f92c3b30b56cb329ab4d0cc270
Unskilled Wolf <geronimo.xl13@gmail.com>
2025-03-30 16:47:12 +0200

    Added Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,10 @@ import Test from "./pages/Test"
 import Nav from "./components/Nav"
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import LoginRegister from "./pages/LoginRegister"
+import Timezone from "./pages/Timezone"
+import UserSettings from "./pages/UserSettings"
 
 function App() {
   // TODO fill in any global app information that we may need here. Potentially use contexts or an additional library.
@@ -13,6 +17,12 @@ function App() {
       {/* All routes will be declared here */}
       <Routes>
         <Route index element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="login" element={<LoginRegister />} />
+        <Route path="register" element={<LoginRegister />} />
+        <Route path="timezone/:zone" element={<Timezone />} />
+        <Route path="userSettings" element={<UserSettings />} />
+        {/* TODO: Remove the test page once the project is further developed */}
         <Route path="test" element={<Test />} />
       </Routes>
       {/* TODO: Fill in any footer information here */}
---
+++ frontend/src/pages/UserSettings.tsx
@@ -0,0 +1,7 @@
+export default function UserSettings() {
+  return (
+    <main id="user-settings">
+      <h1>User Settings</h1>
+    </main>
+  )
+}
