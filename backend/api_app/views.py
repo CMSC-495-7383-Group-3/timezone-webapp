990af6fd40b667517b59ab7a15a5d774a96712b5
alemunsinger <alemnz27@gmail.com>
2025-04-22 20:05:23 +0900

    fix sunset/sunrise route and remove unused decorators from views

--- backend/api_app/views.py
+++ backend/api_app/views.py
@@ -2,12 +2,14 @@ import json
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+
 from .utils import (
     get_sunrise_sunset,
     get_timezone_info,
-    get_time_info_by_timezone_name  # new import
+    get_time_info_by_timezone_name
 )
-from rest_framework import status
 
 class Time_Info(APIView):
     permission_classes = [IsAuthenticated]
@@ -16,36 +18,23 @@ class Time_Info(APIView):
         lat = request.GET.get("lat")
         lon = request.GET.get("lon")
         if not lat or not lon:
-            return Response(json.loads(json.dumps({"error": "Latitude and Longitude are required."})), 
+            return Response({"error": "Latitude and Longitude are required."},
                             status=status.HTTP_400_BAD_REQUEST)
         try:
             sunrise, sunset = get_sunrise_sunset(lat, lon)
             timezone_id, dst, offset = get_timezone_info(lat, lon)
 
-            return Response(
-                json.loads(json.dumps({
-                    "sunrise": sunrise,
-                    "sunset": sunset,
-                    "timezone": timezone_id,
-                    "dstOffset": dst,
-                    "rawOffset": offset
-                })),
-            status=status.HTTP_200_OK)
+            return Response({
+                "sunrise": sunrise,
+                "sunset": sunset,
+                "timezone": timezone_id,
+                "dstOffset": dst,
+                "rawOffset": offset
+            }, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response(json.loads(json.dumps({"error": str(e)})), 
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        return Response({
-            "sunrise": sunrise,
-            "sunset": sunset,
-            "timezone": timezone_id,
-            "dstOffset": dst,
-            "rawOffset": offset
-        })
-    except Exception as e:
-        return Response({"error": str(e)}, status=500)
 
-#  New view for timezone name input
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_time_info_by_timezone(request):
